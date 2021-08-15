867f00e1d7f3d5de8998d8dcb2ac8bdb5a86d4c5
Jakob Magnusson <j@jakes.se>
2021-08-15 21:33:23 +0200

    Removed gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,17 +15,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-      },
-    },
     'gatsby-plugin-offline'
   ],
 }
