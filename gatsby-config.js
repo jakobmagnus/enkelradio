1e87ce7637200d2af5204c4cc821f93d529a0180
Jakob Magnusson <j@jakes.se>
2021-08-15 22:59:03 +0200

    Update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,18 +15,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        //icon: `src/images/gatsby-icon.png`, // TODO: add own icon.
-      },
-    },
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
   ],
 }
